10fb56b1d996a0a2e70be802c6d32d8148437d8c
nancho17 <her.dar.ferre@gmail.com>
2022-05-27 23:57:19 -0300

    feat: buffer generation test

--- serverthread.cpp
+++ serverthread.cpp
@@ -3,29 +3,75 @@
 ServerThread::ServerThread(QTcpServer *serv)
 {
     socket = serv->nextPendingConnection();
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &ServerThread::sendData);
+    sampleTime=0;
+    timer->start(50);
+    connect(socket, SIGNAL(bytesWritten(qint64)),this, SLOT(bytesWritten(qint64)) );
+
+
     //QTcpSocket *socket=server->nextPendingConnection();
 }
 
 
+#define S_TIME 0.001 //seconds
 void ServerThread::run(){
     qDebug()<<"Thread starting";
 
     qDebug()<< "A\n\r";
-    quint32 variable_to_graph=44;
-    while (true) {
-        QByteArray data ((char *) &(variable_to_graph), 4);
+    double variable_to_graph=0;
+  
+    if (false) {
+
+        variable_to_graph = 1.4142* qSin((sampleTime) * 50 * 2 * M_PI );
+        QByteArray data ((char *) &(variable_to_graph), sizeof(double));
+        //qDebug()<< data;
         socket->write(data);
         socket->flush();
-        variable_to_graph++;
-        qDebug()<< data;
-        msleep(1000);
+
+        //sample_time=sample_time+S_TIME;
+        msleep(200);
     }
+
+    /*
     socket->waitForBytesWritten(3000);
     msleep(1000);
     socket->write("Bye\n\r");
     socket->flush();
     socket->waitForBytesWritten(3000);
     socket->close();
+*/
+    exec();
+}
+
+void ServerThread::sendData(){
+
+    //Paralel Gen
+    for (size_t i = 0; i < 9; i++)
+    {
+    image.append (1.4142* qSin((sampleTime) * 50 * 2 * M_PI ));
+    sampleTime=sampleTime+S_TIME;
+    }
+    //Protect
+    //dataPacket.image_var[0]=1.6;
+    //dataPacket.image_var[1]=2.7;
+    //dataPacket.image_var[2]=3.8;
+    //dataPacket.image_var[3]=4.9; image.size()
+    memcpy( (char *) dataPacket.image_var, (char *) image.data(), image.size()*sizeof(double));
+    dataPacket.validCount=image.size();
+    image.clear();
+    //qDebug()<<dataPacket.image_var;
+    //Release
+
+    QByteArray data ((char *) &dataPacket, sizeof(protocolStruct ));
+
+    socket->write(data);
+    socket->flush();
+
 
 }
 
+void ServerThread::bytesWritten(qint64 nBytes){
+    qDebug()<<nBytes;
+}
--- serverthread.h
+++ serverthread.h
@@ -6,16 +6,34 @@
 #include <QThread>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <QTimer>
 
+
+
+
+typedef struct {
+    uint8_t validCount;
+    double image_var[10];
+}protocolStruct;
 
 class ServerThread : public QThread
 {
     Q_OBJECT
 public:
-    ServerThread(QTcpServer*serv);
+    ServerThread(QTcpServer *serv);
     void run();
+    void sendData();
     //
     QTcpSocket *socket;
+
+public slots:
+    void bytesWritten(qint64);
+
+private:
+    QTimer *timer;
+    double sampleTime;
+    protocolStruct dataPacket;
+    QVector <double> image;
 };
 
 #endif // SERVERTHREAD_H
